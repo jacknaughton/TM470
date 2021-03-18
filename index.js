49c0d423371d1d3d4c48e46eb7df2657cfb0b1a7
Jack Naughton <jack.w.naughton@gmail.com>
2021-03-18 12:23:17 +0000

    created show route for id finding

--- index.js
+++ index.js
@@ -3,48 +3,60 @@ var app = express();
 var bodyParser = require("body-parser");
 
 var mongoose = require("mongoose");
+//Offline database.
 // mongoose.connect("mongodb://localhost:27017/products", {
 //   useNewUrlParser: true,
 // });
 
-const uri = "mongodb+srv://jacknaughton1:34Bxo1WBDhxxXara@tm470.rorwa.mongodb.net/project?retryWrites=true&w=majority";
-mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true}, function(err, db) {
-    if (err) { console.log(err); } 
-    else { console.log("Connected to database");}
-});
+//Cloud database.
+const uri =
+  "mongodb+srv://jacknaughton1:34Bxo1WBDhxxXara@tm470.rorwa.mongodb.net/project?retryWrites=true&w=majority";
+mongoose.connect(
+  uri,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  function (err, db) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to database");
+    }
+  }
+);
 
 //Schema setup
 var productSchema = new mongoose.Schema({
   name: String,
   image: String,
+  description: String,
 });
 
 var Product = mongoose.model("Product", productSchema);
-Product.create(
-  {
-  name: "cpu",
-  image: "https://i.ebayimg.com/images/g/Ox4AAOSwEYNfBb~s/s-l300.jpg",
-}, function(e, product){
-  if(e){
-    console.log(e);
-  } else {
-    console.log("New product created: ");
-    console.log(product)
-  }
-});
+// Product.create(
+//   {
+//   name: "cpu",
+//   image: "https://i.ebayimg.com/images/g/Ox4AAOSwEYNfBb~s/s-l300.jpg",
+//   description: "Test description."
+// }, function(e, product){
+//   if(e){
+//     console.log(e);
+//   } else {
+//     console.log("New product created: ");
+//     console.log(product)
+//   }
+// });
 
-Product.create(
-  {
-  name: "ram",
-  image: "https://images-na.ssl-images-amazon.com/images/I/419SRJu4kHL._AC_.jpg",
-}, function(e, product){
-  if(e){
-    console.log(e);
-  } else {
-    console.log("New product created: ");
-    console.log(product)
-  }
-});
+// Product.create(
+//   {
+//   name: "ram",
+//   image: "https://images-na.ssl-images-amazon.com/images/I/419SRJu4kHL._AC_.jpg",
+// }, function(e, product){
+//   if(e){
+//     console.log(e);
+//   } else {
+//     console.log("New product created: ");
+//     console.log(product)
+//   }
+// });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
@@ -57,7 +69,7 @@ app.get("/", function (req, res) {
   res.render("home");
 });
 
-//Sends you to the products page.
+//Index route: Sends you to the products page.
 app.get("/products", function (req, res) {
   //Get all products from the database.
   Product.find({}, function (e, allProducts) {
@@ -69,11 +81,12 @@ app.get("/products", function (req, res) {
   });
 });
 
-//Create new product object.
+//Create route: Create new product object.
 app.post("/products", function (req, res) {
   var name = req.body.name;
   var image = req.body.image;
-  var newProduct = { name: name, image: image };
+  var description = req.body.description;
+  var newProduct = { name: name, image: image, description: description };
   //Create a new product then save it to the database.
   Product.create(newProduct, function (e, createdProduct) {
     if (e) {
@@ -82,12 +95,23 @@ app.post("/products", function (req, res) {
       res.redirect("/products");
     }
   });
-  // res.redirect("/products");
 });
 
-//Shows form to create new product object
+//New route: Shows form to create new product object
 app.get("/products/new", function (req, res) {
   res.render("new");
+});
+
+//Show route: Render show template with given id.
+app.get("/products/:id", function (req, res) {
+  //Find product with given id.
+  Product.findById(req.params.id, function (e, foundProduct) {
+    if (e) {
+      console.log(e);
+    } else {
+      res.render("show", { products: foundProduct });
+    }
+  });
 });
 
 //Starts the server.
