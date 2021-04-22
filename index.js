fb48e676fe8f90444ae0891f7fc8c1d51951bc4f
Jack Naughton <jack.w.naughton@gmail.com>
2021-04-22 16:37:58 +0100

    further comment development

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require("express");
-var app = express();
+var index = express();
 var bodyParser = require("body-parser");
 
 var mongoose = require("mongoose");
 
 var Product = require("./models/products");
+var Comment = require("./models/comment");
 
 var seedDB = require("./views/seeds");
+const { resolveInclude } = require("ejs");
 
 //Cloud database.
 const uri =
@@ -25,31 +27,31 @@ mongoose.connect(
 
 seedDB();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
+index.use(bodyParser.urlencoded({ extended: true }));
+index.use(express.static(__dirname + "/public"));
 
 //Set all routes to ejs files.
-app.set("view engine", "ejs");
+index.set("view engine", "ejs");
 
 //Home page route.
-app.get("/", function (req, res) {
+index.get("/", function (req, res) {
   res.render("home");
 });
 
 //Index route: Sends you to the products page.
-app.get("/products", function (req, res) {
+index.get("/products", function (req, res) {
   //Get all products from the database.
   Product.find({}, function (e, allProducts) {
     if (e) {
       console.log(e);
     } else {
-      res.render("products", { products: allProducts });
+      res.render("products/products", { products: allProducts });
     }
   });
 });
 
 //Create route: Create new product object.
-app.post("/products", function (req, res) {
+index.post("/products", function (req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var description = req.body.description;
@@ -65,23 +67,54 @@ app.post("/products", function (req, res) {
 });
 
 //New route: Shows form to create new product object
-app.get("/products/new", function (req, res) {
-  res.render("new");
+index.get("/products/new", function (req, res) {
+  res.render("products/new");
 });
 
 //Show route: Render show template with given id.
-app.get("/products/:id", function (req, res) {
+index.get("/products/:id", function (req, res) {
   //Find product with given id.
-  Product.findById(req.params.id).populate("comments").exec(function (e, foundProduct) {
+  Product.findById(req.params.id)
+    .populate("comments")
+    .exec(function (e, foundProduct) {
+      if (e) {
+        console.log(e);
+      } else {
+        res.render("products/show", { products: foundProduct });
+      }
+    });
+});
+
+index.get("/products/:id/comments/new", function (req, res) {
+  Product.findById(req.params.id, function (e, products) {
     if (e) {
       console.log(e);
     } else {
-      res.render("show", { products: foundProduct });
+      res.render("comments/new", {products: products});
     }
   });
 });
 
+index.post("/products/:id/comments", function(req,res){
+  Product.findById(req.params.id, function(e, product){
+    if(e){
+      console.log(e);
+      res.redirect("/products");
+    } else {
+      Comment.create(req.body.comment, function(e, comment){
+        if(e){
+          console.log(e);
+        } else{
+          product.comments.push(comment);
+          product.save();
+          res.redirect("/products/" + product._id);
+        }
+      })
+    }
+  })
+})
+
 //Starts the server.
-app.listen(3000, function () {
+index.listen(3000, function () {
   console.log("Server started.");
 });
